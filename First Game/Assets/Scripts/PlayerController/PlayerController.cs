b84381b484c9f07067bf16a40dc313b8a4d7e702
Jepps42 <63672521+Jepps42@users.noreply.github.com>
2025-09-05 12:36:02 -0400

    Wrote the playercontroller

--- First Game/Assets/Scripts/PlayerController/PlayerController.cs
+++ First Game/Assets/Scripts/PlayerController/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D RB;
+
+    private float Speed = 8;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 vel = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.A))
+        {
+            vel.x = -Speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            vel.y = -Speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            vel.x = Speed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            vel.y = Speed;
+        }
         
+        RB.velocity = vel;
     }
 }
